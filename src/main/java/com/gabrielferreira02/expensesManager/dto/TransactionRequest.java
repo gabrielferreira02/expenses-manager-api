5c19cac8e6de274786d13d30698ea966fc93254a
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2025-01-11 12:21:42 -0300

    Update TransactionRequest - Added AllArgsConstructor annotation

--- src/main/java/com/gabrielferreira02/expensesManager/dto/TransactionRequest.java
+++ src/main/java/com/gabrielferreira02/expensesManager/dto/TransactionRequest.java
@@ -3,11 +3,13 @@ package com.gabrielferreira02.expensesManager.dto;
 import com.gabrielferreira02.expensesManager.entity.TransactionType;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.util.UUID;
 
 @Getter
+@AllArgsConstructor
 public class TransactionRequest {
     @NotNull
     private UUID id;
